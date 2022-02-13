d75bf29d7dd536744a0519d459585a4e87f6a7c8
asmaalqarzaie <48890238+asmaalqarzaie@users.noreply.github.com>
2022-02-13 15:37:07 +0300

    Movies by search

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ const apiKey= `085893799663f0096f9d370baf640f22`;
 
 let watchedList=[];
 let favoriteList=[];
-//const imgPath=
-//
+
+
+
+
+//-------------------------const imgPath----------------------//
+
 let actionCard= document.getElementById("Action");
 moviesCat(28,actionCard)
 let comedyCard= document.getElementById("Comedy");
@@ -36,3 +40,30 @@ function moviesCat(id,card){
 
   })
 }
+
+
+//------------------ Get Movies by search ------------------------
+const form = document.querySelector(".bysearch form");
+form.addEventListener("submit",(event)=>{
+  event.preventDefault();
+  let myInput = document.querySelector("#myInput").value;
+  console.log(myInput);
+  axios
+  .get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adlt=false&query=${myInput}`)
+  .then ((res) =>{ console.log(res.data.results)
+    document.getElementById("searchedMovie").innerHTML = res.data.results.map(item=>
+      `<div class="col-6 col-md-3 mb-4">
+      <div class="card border-0">
+      <img src=${imgpath+item.poster_path}>
+      <div class="text-center bg-black justify-content-md-between align-items-center">
+      <a class="btn test"data-bs-toggle="modal"data-bs-target="#theModalBox" onclick="MoviesDetails(${item.id}">Details</a>
+      <a><i class="far fa-heart btn p-1 test" onclick="FavList(${item.id}"></i></a>
+      <a><i class="fas fa-plus btn p-1 test" onclick="watchList(${item.id}"></i></a>
+      </div>
+      </div>
+      </div>`
+    ).join('')
+})
+})
+
+//-----------------------Get Trending Movies---------------------------------
